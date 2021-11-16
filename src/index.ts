772220113561706b554dba6bbd365d361ec6de20
Dmitri Samo <unicatintern@gmail.com>
2021-11-16 21:58:47 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,66 @@
-import { QMainWindow, QWidget, QLabel, FlexLayout, QPushButton, QIcon } from '@nodegui/nodegui';
-import logo from '../assets/logox200.png';
+import {
+  QMainWindow,
+  QWidget,
+  QLabel,
+  FlexLayout,
+  QPushButton,
+  QIcon,
+  FileMode,
+} from "@nodegui/nodegui";
+import { exec } from "child_process";
+import logo from "../assets/logox200.png";
 
 const win = new QMainWindow();
-win.setWindowTitle("Hello World");
+win.setWindowTitle("DMS Easy Git");
 
 const centralWidget = new QWidget();
 centralWidget.setObjectName("myroot");
 const rootLayout = new FlexLayout();
 centralWidget.setLayout(rootLayout);
 
-const label = new QLabel();
-label.setObjectName("mylabel");
-label.setText("Hello");
+const browseFolder = new QLabel();
+browseFolder.setObjectName("browselabel");
+browseFolder.setText("Hello");
+
+const { QFileDialog } = require("@nodegui/nodegui");
+
+const browseButton = new QPushButton();
+browseButton.setText("Browse Project Directory");
+browseButton.addEventListener("clicked", () => {
+  const fileDialog = new QFileDialog();
+  fileDialog.setFileMode(FileMode.Directory);
+
+  const selectedFiles = fileDialog.selectedFiles();
+  fileDialog.exec();
+  console.log(selectedFiles);
+
+  var cmdCD = require("node-cmd");
+  var check = cmdCD.run(`cd ` + selectedFiles);
+
+  console.log(check);
+
+  // function (error, stdout, stderr) {
+  //   if (error) browseButton.setText(error);
+  //   console.log("stdout: " + stdout);
+  // }
+});
+
+const buttonGitAdd = new QPushButton();
+buttonGitAdd.setText("Git Add .");
+var cmdGitAdd = require("node-cmd");
+var add = cmdGitAdd.run(`git add .`);
+console.log(add);
+
+const buttonCommit = new QPushButton();
+buttonCommit.setText("Git Commit");
+var cmdCommit = require("node-cmd");
+var commit = cmdCommit.run(`git commit -m "test"`);
+console.log(commit);
+
+const buttonPush = new QPushButton();
+buttonPush.setText("Git Push");
+var cmdPush = require("node-cmd");
+var push = cmdPush.run(`git push`);
 
 const button = new QPushButton();
 button.setIcon(new QIcon(logo));
@@ -22,8 +71,11 @@ label2.setInlineStyle(`
   color: red;
 `);
 
-rootLayout.addWidget(label);
-rootLayout.addWidget(button);
+rootLayout.addWidget(browseFolder);
+rootLayout.addWidget(browseButton);
+rootLayout.addWidget(buttonGitAdd);
+rootLayout.addWidget(buttonCommit);
+rootLayout.addWidget(buttonPush);
 rootLayout.addWidget(label2);
 win.setCentralWidget(centralWidget);
 win.setStyleSheet(
