d5a4c5b0c44dac7b7880fc72512590dd09fb4682
Dmitri Samo <unicatintern@gmail.com>
2021-11-22 20:57:42 +0100

    final fix

--- src/index.ts
+++ src/index.ts
@@ -195,7 +195,7 @@ const versionLabel = new QLabel();
 versionLabel.setInlineStyle(
   "font-size:12; font-weight: bold; padding: 4; margin: 4;"
 );
-versionLabel.setText("Version: 1.0.5");
+versionLabel.setText("Version: 1.0.0");
 
 rootLayout.addWidget(labelImage);
 rootLayout.addWidget(buttonSkin);
